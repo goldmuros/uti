df400731b6c1d7ffe86e96291c50c8acd5960c9d
Esteban <goldmuros@yahoo.com>
2019-09-12 09:27:56 -0300

    Minor change

--- src/config/config-firebase.js
+++ src/config/config-firebase.js
@@ -2,20 +2,12 @@ import firebase from 'firebase'
 import 'firebase/firestore'
 
 const config_firebase = {
-  apiKey: 'AIzaSyALfkNVI8TJLw9FvSYzDE7SMIFIoV6QQ6w',
-  authDomain: 'uti-terapia.firebaseapp.com',
-  databaseURL: 'https://uti-terapia.firebaseio.com',
-  projectId: 'uti-terapia',
-  storageBucket: 'uti-terapia.appspot.com'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: ''
 }
-
-// const config_firebase = {
-//   apiKey: '',
-//   authDomain: '',
-//   databaseURL: '',
-//   projectId: '',
-//   storageBucket: ''
-// }
 
 firebase.initializeApp(config_firebase)
 
